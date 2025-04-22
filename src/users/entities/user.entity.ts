1acb17d443fffcfca4c958922d2a78b1c6943c0e
Pedro Braghin <braghindev@gmail.com>
2025-04-22 12:43:56 -0300

    feat(user): add user entities

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+} from 'typeorm';
+
+@Entity()
+@TableInheritance({ column: { type: 'varchar', name: 'type' } })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+}
