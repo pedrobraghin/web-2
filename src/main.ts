4d83c216b8873c1dfeb53de3bfc8f6bdfcd94e1c
Pedro Braghin <braghindev@gmail.com>
2025-04-22 11:16:20 -0300

    chore(deps): config class transformer and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Events Manager')
+    .setDescription('An API to manage events and their attendees')
+    .setVersion('1.0')
+    .addTag('events')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT);
 }
+
 bootstrap();
